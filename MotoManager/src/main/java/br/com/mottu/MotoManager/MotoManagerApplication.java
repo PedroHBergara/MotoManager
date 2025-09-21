3ce90527c930cfa27cba2504f100a511385246d5
PedroHBergara <pedrobergara5@gmail.com>
2025-09-20 22:45:59 -0300

    versao 0

---
+++ MotoManager/src/main/java/br/com/mottu/MotoManager/MotoManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mottu.MotoManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoManagerApplication.class, args);
+	}
+
+}
---
+++ MotoManager/src/test/java/br/com/mottu/MotoManager/MotoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mottu.MotoManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
